d44ae1b8b7c5522bdde30e20dd4e86a244e39195
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-07-24 13:58:49 +0430

    add str utility for manipulate strings

---
+++ stringUtility/stringUtility.go
@@ -0,0 +1,46 @@
+package str
+
+import (
+	"fmt"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
+)
+
+type String string
+
+// todo: write test for these functions
+
+func (str String) Bold() String {
+	return String(fmt.Sprintf("<b>%s</b>", str))
+}
+
+func (str String) Strike() String {
+	return String(fmt.Sprintf("<s>%s</s>", str))
+}
+
+func (str String) UnderLine() String {
+	return String(fmt.Sprintf("<u>%s</u>", str))
+}
+
+func (str String) ToLink(url string) String {
+	return String(fmt.Sprintf(`<a href="%s">%s</a>`, url, str))
+}
+
+func (str String) AddNewLine() String {
+	return String(fmt.Sprintf("%+v \n", str))
+}
+
+func (str String) Append(str2 string) String {
+	return String(fmt.Sprintf("%+v %+v", str, str2))
+}
+
+func (str String) ToString() string {
+	return string(str)
+}
+
+type Number int
+
+func (num Number) AddComma() String {
+	p := message.NewPrinter(language.English)
+	return String(p.Sprintf("%d", num))
+}
