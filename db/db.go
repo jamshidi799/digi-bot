1f7f20d0b696f1729fc2dccce6814f6ef97ed193
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-08-06 22:33:59 +0430

    3 try to connect to db

--- db/db.go
+++ db/db.go
@@ -8,6 +8,7 @@ import (
 	"gorm.io/gorm"
 	"log"
 	"os"
+	"time"
 )
 
 var DB *gorm.DB
@@ -23,12 +24,22 @@ func Init() *gorm.DB {
 	DbHost := os.Getenv("DB_HOST")
 
 	dsn := fmt.Sprintf("root:%s@tcp(%s:3306)/digiBot?charset=utf8mb4&parseTime=True&loc=Local", DbPassword, DbHost)
-	println(dsn)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//println(dsn)
+
+	for i := 1; i <= 3; i++ {
+		log.Printf("connect to db try %d", i)
+		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			log.Println(err)
+		}
+
+		time.Sleep(time.Second * 2)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	DB = db
+
 	migration()
 	return DB
 }
