ffc2b5565f070fd64c71fd935daaa4cc8d36258a
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-08-06 15:33:18 +0430

    add env file for db config

--- db/db.go
+++ db/db.go
@@ -2,15 +2,28 @@ package db
 
 import (
 	"digi-bot/model"
+	"fmt"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func Init() *gorm.DB {
-	dsn := "root:mohammad79@tcp(127.0.0.1:3306)/digiBot?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbHost := os.Getenv("DB_HOST")
+
+	dsn := fmt.Sprintf("root:%s@tcp(%s:3306)/digiBot?charset=utf8mb4&parseTime=True&loc=Local", DbPassword, DbHost)
+	println(dsn)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/prprprus/scheduler v0.5.0
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
