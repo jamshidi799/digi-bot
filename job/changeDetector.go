4364533a32e5ca90924f203e27a2255247563f38
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-08-21 01:42:35 +0430

    refactor: extract job from change detector

---
+++ job/changeDetector.go
@@ -0,0 +1,68 @@
+package job
+
+import (
+	"digi-bot/crawler"
+	"digi-bot/db"
+	"digi-bot/messageCreator"
+	"digi-bot/model"
+	productService "digi-bot/service/product"
+	userService "digi-bot/service/user"
+	"log"
+	"time"
+)
+
+func ChangeDetector() {
+	for {
+		log.Printf("--------------- Scheduler --------------------\n")
+
+		updateCount := refresh()
+
+		log.Printf("compare finished. num of update: %d \n", updateCount)
+		log.Printf("--------------- done --------------------\n")
+
+		time.Sleep(time.Hour * 2)
+	}
+}
+
+func refresh() int {
+	updateCount := 0
+	products := db.GetAllProduct()
+
+	for _, product := range products {
+		newProduct, err := crawler.Crawl(product.Url)
+		if err != nil {
+			log.Println(newProduct)
+			continue
+		}
+
+		if message, isChanged, changeLevel := compare(newProduct, product.ToDto()); isChanged {
+			log.Printf("old price: %d, new price: %d",
+				product.Price,
+				newProduct.Price)
+			userService.SendProductUpdateToUsers(product.ID, message, changeLevel)
+			productService.UpdateProduct(product, newProduct)
+			updateCount++
+		}
+
+		//break
+		time.Sleep(time.Second * 3)
+	}
+	return updateCount
+}
+
+func compare(newProduct model.ProductDto, oldProduct model.ProductDto) (message string, isChanged bool, changeLevel int) {
+	if newProduct.Price == oldProduct.Price {
+		return "", false, 0
+	}
+
+	if newProduct.Price == 0 {
+		return messageCreator.CreateNotAvailableMsg(newProduct), true, 1
+	}
+
+	changeLevel = 1
+	if newProduct.Status == 2 {
+		changeLevel = 2
+	}
+
+	return messageCreator.CreateNormalPriceChangeMsg(newProduct, newProduct.Price, oldProduct.Price), true, changeLevel
+}
--- main.go
+++ main.go
@@ -4,13 +4,9 @@ import (
 	"digi-bot/bot"
 	"digi-bot/crawler"
 	"digi-bot/db"
-	"digi-bot/messageCreator"
-	"digi-bot/model"
-	productService "digi-bot/service/product"
-	userService "digi-bot/service/user"
+	"digi-bot/job"
 	"log"
 	"sync"
-	"time"
 )
 
 func main() {
@@ -24,59 +20,6 @@ func main() {
 	//go crawler.BatchCrawlScheduler()
 	go crawler.StartBatchCrawl()
 	group.Add(1)
-	for {
-		Scheduler()
-		time.Sleep(time.Hour * 2)
-		//time.Sleep(time.Second * 10)
-	}
-
+	job.ChangeDetector()
 	group.Wait()
-}
-
-func Scheduler() {
-	products := db.GetAllProduct()
-
-	log.Printf("--------------- Scheduler --------------------\n")
-	updateCount := 0
-
-	for _, product := range products {
-		newProduct, err := crawler.Crawl(product.Url)
-		if err != nil {
-			log.Println(newProduct)
-			continue
-		}
-
-		if message, isChanged, changeLevel := changeDetector(newProduct, product.ToDto()); isChanged {
-			log.Printf("old price: %d, new price: %d",
-				product.Price,
-				newProduct.Price)
-			userService.SendProductUpdateToUsers(product.ID, message, changeLevel)
-			productService.UpdateProduct(product, newProduct)
-			updateCount++
-		}
-
-		//break
-		time.Sleep(time.Second * 3)
-	}
-
-	log.Printf("changeDetector finished. num of update: %d \n", updateCount)
-	log.Printf("--------------- done --------------------\n")
-
-}
-
-func changeDetector(newProduct model.ProductDto, oldProduct model.ProductDto) (message string, isChanged bool, changeLevel int) {
-	if newProduct.Price == oldProduct.Price {
-		return "", false, 0
-	}
-
-	if newProduct.Price == 0 {
-		return messageCreator.CreateNotAvailableMsg(newProduct), true, 1
-	}
-
-	changeLevel = 1
-	if newProduct.Status == 2 {
-		changeLevel = 2
-	}
-
-	return messageCreator.CreateNormalPriceChangeMsg(newProduct, newProduct.Price, oldProduct.Price), true, changeLevel
 }
