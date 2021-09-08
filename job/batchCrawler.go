4ef80a975b9771ded74c0fec640a92eabe25e67e
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-09-08 17:34:18 +0430

    run bulk history every three days

--- job/batchCrawler.go
+++ job/batchCrawler.go
@@ -9,7 +9,7 @@ import (
 func BatchCrawlerJob() {
 	cTab := crontab.New()
 
-	err := cTab.AddJob("0 0 * * *", crawler.DigikalaCrawler{}.BatchCrawl) // on 1st day of month
+	err := cTab.AddJob("0 0 */3 * *", crawler.DigikalaCrawler{}.BatchCrawl) // on 1st day of month
 	if err != nil {
 		log.Println(err)
 		return
