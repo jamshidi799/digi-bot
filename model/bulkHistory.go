a2720dd8598a8df259715725508edfcd96eab951
jamshidi.m799 <jamshidi.m799@gmail.com>
2021-11-09 00:37:01 +0330

    create index for product id in bulk history table

--- model/bulkHistory.go
+++ model/bulkHistory.go
@@ -4,7 +4,7 @@ import "time"
 
 type BulkHistory struct {
 	ID              int `gorm:"primaryKey"`
-	SourceProductID int
+	SourceProductID int `gorm:"index"`
 	CategoryID      int
 	BrandID         int
 	Price           int
