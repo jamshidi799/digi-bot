0e65999ecee30b8e419f67f650fa1015a60115ea
Mohammad <jamshidi.m799@gmail.com>
2022-10-11 19:57:39 +0330

    remove grpc and protobuf

--- digi-bot/service/search/client.go
+++ digi-bot/service/search/client.go
@@ -1,38 +1,29 @@
 package search
 
-import (
-	"context"
-	"digi-bot/model"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"log"
-	"time"
-)
-
-func Query(term string) *SearchResponse {
-	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-	defer conn.Close()
-	c := NewSearchClient(conn)
-
-	// Contact the server and print out its response.
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
-	defer cancel()
-	r, err := c.Search(ctx, &SearchRequest{Term: term})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	return r
-}
-
-func (p *Product) ToDto() model.ProductDto {
-	return model.ProductDto{
-		Id:    int(p.Id),
-		Name:  p.Title,
-		Url:   p.Url,
-		Price: int(p.Price),
-		Sku:   p.Sku,
-	}
-}
+//func Query(term string) *SearchResponse {
+//	conn, err := grpc.Dial("localhost:8080", grpc.WithTransportCredentials(insecure.NewCredentials()))
+//	if err != nil {
+//		log.Fatalf("did not connect: %v", err)
+//	}
+//	defer conn.Close()
+//	c := NewSearchClient(conn)
+//
+//	// Contact the server and print out its response.
+//	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+//	defer cancel()
+//	r, err := c.Search(ctx, &SearchRequest{Term: term})
+//	if err != nil {
+//		log.Fatalf("could not greet: %v", err)
+//	}
+//	return r
+//}
+//
+//func (p *Product) ToDto() model.ProductDto {
+//	return model.ProductDto{
+//		Id:    int(p.Id),
+//		Name:  p.Title,
+//		Url:   p.Url,
+//		Price: int(p.Price),
+//		Sku:   p.Sku,
+//	}
+//}
